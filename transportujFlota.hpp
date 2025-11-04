983612930d9589a4d6a5b7ad25d2f96ae1d837fe
xSzumielx <szumielbartek@onet.pl>
2025-11-04 18:28:57 +0100

    Zrobiona zejściówka

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,22 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) { return 0; }
+
+    Stocznia stocznia{};
+    unsigned int liczba_zaglowcow = 0;
+    unsigned int x = 0;
+
+    while (x < towar)
+    {
+        Statek* s1 = stocznia();
+        unsigned int y = s1->transportuj();
+        x = x + y;
+        if (dynamic_cast<Zaglowiec*>(s1))
+        {
+            ++liczba_zaglowcow;
+        }
+        delete s1;
+    }
+    return liczba_zaglowcow;
 }
